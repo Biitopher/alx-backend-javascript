b0c4509587ef72a2a5fc7dbee3d49e94f90e2368
Biitopher <biitopher@gmail.com>
2024-01-03 23:14:43 +0000

    Task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,4 @@
-const { uploadPhoto, createUser } = require('./utils.js');
-
+import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([body, fullName]) => console.log(`${body.body} ${fullName.firstName} ${fullName.lastName}`))
