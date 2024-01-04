9844ec2e5e42fd133791c065da1119f06329984f
Biitopher <biitopher@gmail.com>
2024-01-04 20:52:56 +0000

    Task 6

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -11,21 +11,5 @@ class Building {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
- this.sqft = sqft;
-  }
 
-  /**
-   * @param {Number} sqft
-   */
-  set sqft(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft nust be a number');
-    }
-    this._sqft = sqft;
-  }
-
-  get sqft() {
-    return this._sqft;
-  }
-}
 export default Building;
