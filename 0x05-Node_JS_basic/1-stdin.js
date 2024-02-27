b6c22b4d2f482d8b8963cec0f201ed3c7eb901ba
Biitopher <biitopher@gmail.com>
2024-02-27 23:17:05 +0300

    Lint fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
+process.stdin.on('readable', () = > {
   const readline= process.stdin.read();
 
   if (readline) {
@@ -8,6 +8,6 @@ process.stdin.on('readable', () => {
   }
 });
 
-process.stdin.on('close', () => {
+process.stdin.on('close', () = > {
   process.stdout.write('This important software is now closing\n');
 });
