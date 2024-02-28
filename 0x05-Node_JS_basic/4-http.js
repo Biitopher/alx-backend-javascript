d2d3ba15d4fe57962e1ece659005c2b4c4c250d5
Biitopher <biitopher@gmail.com>
2024-02-28 11:25:27 +0300

    Lint fix

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,12 @@
 const http = require('http');
+
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer((request, result) => {
-  result.statusCode = 200;
-  result.setHeader('Content-Type', 'text/plain');
-  result.end('Hello Holberton School!');
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port, hostname, () => { });
