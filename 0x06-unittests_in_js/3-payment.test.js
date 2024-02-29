e9a0ad828319a08f68dc8fced61b1fed44f2ebe7
Biitopher <biitopher@gmail.com>
2024-02-29 15:35:00 +0300

    Task 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,11 +1,11 @@
 const {describe, it} = require('mocha');
 const sinon = require('sinon');
-const { sendPaymentRequestToApi } = require('./3-payment');
+const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 const assert = require("assert");
 
-describe('sendPaymentRequestToApi', function() => {
-  it('should call Utils.calculateNumber with the correct arguments and log the result', function() => {
+describe('sendPaymentRequestToApi', function() {
+  it('should call Utils.calculateNumber with the correct arguments and log the result', function() {
     const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
 
     sendPaymentRequestToApi(100, 20);
