47315f8941ad9fc64c41cada8d237830be52ed33
Biitopher <biitopher@gmail.com>
2024-02-29 12:35:00 +0300

    Task 1

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -2,19 +2,16 @@ function calculateNumber(type, a, b) {
   const roundedA = Math.round(a);
   const roundedB = Math.round(b);
 
-  switch (type) {
-    case 'SUM':
-      return roundedA + roundedB;
-    case 'SUBTRACT':
-      return roundedA - roundedB;
-    case 'DIVIDE':
-      if (roundedB === 0) {
-        return 'Error';
-      }
-      return roundedA / roundedB;
-    default:
-      throw new Error('Invalid type. Use SUM, SUBTRACT, or DIVIDE.');
-  }
-}
+     if (type === 'SUM') {
+        return roundedA + roundedB;
+     }
+     if (type === 'SUBTRACT') {
+        return roundedA - roundedB;
+     }
+     if (type === 'DIVIDE') {
+        return  roundedB === 0 ? 'Error' : roundedA / roundedB;
+     }
+     return 0;
+};
 
 module.exports = calculateNumber;
