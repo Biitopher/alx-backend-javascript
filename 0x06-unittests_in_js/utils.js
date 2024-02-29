76e518ce43f8671e1839dcbd07546005c3432a7b
Biitopher <biitopher@gmail.com>
2024-02-29 15:41:21 +0300

    Task 3

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,25 +1,25 @@
 class Utils {
     static calculateNumber(type, a, b) {
-    const roundedA = Math.round(a);
-    const roundedB = Math.round(b);
-    let x = 0;
-    switch (type) {
-    case 'SUM':
-      x = roundedA + roundedB;
-      break;
-    case 'SUBTRACT':
-      x = roundedA - roundedB;
-      break;
-    case 'DIVIDE':
-      if (roundedB === 0) {
-        x = 'Error';
-      } else {
-            x = roundedA / roundedB;
-      }
-      break;
-      }
-      return x;
-  }
+        const roundedA = Math.round(a);
+        const roundedB = Math.round(b);
+        let x = 0;
+        switch (type) {
+        case 'SUM':
+            x = roundedA + roundedB;
+            break;
+        case 'SUBTRACT':
+            x = roundedA - roundedB;
+            break;
+        case 'DIVIDE':
+            if (roundedB === 0) {
+            x = 'Error';
+            } else {
+                x = roundedA / roundedB;
+            }
+            break;
+        }
+        return x;
+    }
 }
 
 module.exports = Utils;
