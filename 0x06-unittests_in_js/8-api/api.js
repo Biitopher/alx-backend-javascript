09418ed218448dec2d6f14cbb77b683fb46db17c
Biitopher <biitopher@gmail.com>
2024-02-29 21:15:37 +0300

    Task 8

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -4,11 +4,11 @@ const app = express();
 const PORT = 7865;
 
 app.get('/', (req, res) => {
-  res.status(200).send('Welcome to the payment system');
+  res.send('Welcome to the payment system');
 });
 
 const server = app.listen(PORT, () => {
-  console.log(`API available on http://localhost:${PORT}`);
+  console.log(`API available on localhost port ${PORT}`);
 });
 
-module.exports = server;
+module.exports = app;
