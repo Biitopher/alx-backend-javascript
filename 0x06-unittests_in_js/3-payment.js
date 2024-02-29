601496737fa8bdbef492095af9638ab4efff666f
Biitopher <biitopher@gmail.com>
2024-02-29 16:00:50 +0300

    Task 3

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,8 @@
 const Utils = require('./utils');
 
-function sendPaymentRequestToApi = (totalAmount, totalShipping) {
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const sumResult = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${sumResult}`);
-};
+}
 
 module.exports = { sendPaymentRequestToApi };
