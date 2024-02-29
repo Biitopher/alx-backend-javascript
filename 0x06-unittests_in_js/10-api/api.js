e1af311a6e94f48fe67d0ba4795f9ab2372123a7
Biitopher <biitopher@gmail.com>
2024-03-01 00:06:12 +0300

    Task 10

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 7865;
 
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.get('/', (_req, res) => {
   res.send('Welcome to the payment system');
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -4,7 +4,7 @@ const { expect } = require('chai');
 describe('API integration test', () => {
   const API_URL = 'http://localhost:7865';
 
-  it('GET / returns correct response', (done) => {
+  it('GET / responds with status code 200', (done) => {
     request.get(`${API_URL}/`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
@@ -12,7 +12,7 @@ describe('API integration test', () => {
     });
   });
 
-  it('GET /cart/:id returns correct response for valid :id', (done) => {
+  it('GET /cart/:id responds with correct result :id', (done) => {
     request.get(`${API_URL}/cart/47`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Payment methods for cart 47');
